841facf01942eff46d835dabbb09a616bacb4e15
Raphaël Valyi <rvalyi@gmail.com>
2013-05-12 17:46:53 -0300

    makes cells view partials available to Rails app where aktooor is mounted

--- lib/aktooor/engine.rb
+++ lib/aktooor/engine.rb
@@ -1,5 +1,15 @@
 module Aktooor
   class Engine < ::Rails::Engine
     isolate_namespace Aktooor
+
+    initializer :add_cells_view_paths do
+      paths.add "app/cells"
+      views = paths["app/cells"].existent
+      unless views.empty?
+        ActiveSupport.on_load(:action_controller){ prepend_view_path(views) }
+        ActiveSupport.on_load(:action_mailer){ prepend_view_path(views) }
+      end
+    end
+
   end
 end
